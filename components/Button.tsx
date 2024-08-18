1b56cb3ecc79f908e386cf315d1b7649a662825c
arvee0220 <rowellverdejo@gmail.com>
2024-08-19 02:17:20 +0800

    Fix Button prop bg_color

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,7 +4,15 @@ const Button: React.FC<ButtonProps> = ({ children, onClick, bg_color }) => {
 	return (
 		<button
 			onClick={onClick}
-			className={`bg-${bg_color}-800 text-white font-medium px-4 py-2 rounded-lg shadow-md mr-2`}
+			className={`${
+				bg_color === "green"
+					? "bg-green-800"
+					: bg_color === "red"
+					? "bg-red-800"
+					: bg_color === "blue"
+					? "bg-blue-800"
+					: ""
+			} text-white font-medium px-4 py-2 rounded-lg shadow-md mr-2`}
 		>
 			{children}
 		</button>
